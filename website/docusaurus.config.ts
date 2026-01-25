408d44baf4715af073c9a8666454db3ad3fdb49f
Oleg Mukhin <oleg.v.mukhin@gmail.com>
2026-01-25 15:38:18 +0000

    feat(docs): add PR preview deployments
    - Deploy PR builds to /pr-preview/{pr_number}/
    - Add bot comment with preview URL on each PR
    - Auto-cleanup preview when PR is merged/closed
    - Support dynamic baseUrl via DOCUSAURUS_BASE_URL env var

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://thoughtgate.io',
-  baseUrl: '/',
+  baseUrl: process.env.DOCUSAURUS_BASE_URL || '/',
 
   organizationName: 'thoughtgate',
   projectName: 'thoughtgate',
